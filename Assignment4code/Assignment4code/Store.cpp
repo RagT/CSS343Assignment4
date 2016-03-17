1b2693ad196cc909bd18930f90d16ab5140a31ef
RAYKation <randyhang92@hotmail.com>
2016-03-17 14:06:36 -0700

    Update Store.cpp

--- Assignment4code/Assignment4code/Store.cpp
+++ Assignment4code/Assignment4code/Store.cpp
@@ -28,6 +28,7 @@ void Store::buildCustomers(ifstream& infile)
 		}
 	}
 }
+
 //----------------- addItems ----------------------
 //Creates Items from text file to add to Store
 void Store::addItems(ifstream & infile)
@@ -89,244 +90,16 @@ void Store::processCommands(ifstream& infile)
 		//H: Display Customer's History
 		else if (actionType == 'H')
 		{
-			//Label for displaying Transactions of a particular Customer
-			cout << "----------------------Customer Transactions------------------" << endl;
-
-			//read in Customer's ID from infile
-			int custID;
-			infile >> custID;
-			
-			//determine if customer exists in the system
-			Customer* temp = getCustomer(custID);
-
-			//check to see if customer exists
-			if (temp != NULL) //YES
-			{
-				//display customer's information
-				cout << "Customer ID: " << custID << " " << temp->getFName() << " " << temp->getLName() << endl;
-
-				//display customer's history
-				displayHistory(custID);
-			}
-			
-			else //NO
-			{
-				//display error message with rejected line
-				cout << "Non-Existent customer entered. Invalid Line: " << "H " << custID << endl;
-			}
-			
-			//gets rid of '\n'
-			infile.get();
+			//process and displays history from txt file
+			processHistory(infile);
 		}
 
-		//B: Set Transaction as borrowed and assign it to the Customer in their history
+		//B: Set Transaction as borrowed, determine which genre 
+		//and assign it to the Customer in their history
 		else if (actionType == 'B')
 		{
-			//read in Customer's ID from infile
-			int custID;
-			infile >> custID;
-			
-			//returns NULL if customer doesn't exist
-			Customer* cust = getCustomer(custID);
-			
-			//check to see if customer exists
-			if (cust != NULL) //YES
-			{
-				//set returned as false
-				rental->setBorrowed();
-
-				//set Customer's ID in the Transaction
-				rental->setCustID(custID);
-				
-				//set the MediaType in the Transaction
-				char media;
-				infile >> media;
-				rental->setMediaType(media);
-
-				//get genre from infile
-				char genre;
-				infile >> genre;
-				
-				//using correct format for Classic Movie
-				if (genre == 'C')
-				{
-					//get month from infile
-					int month;
-					infile >> month;
-
-					//get year from infile
-					int year;
-					infile >> year;
-
-					//get actor first name from txt file
-					string actorF;
-					char character = infile.get();
-					
-					//build actor's first name until space
-					while (character != ' ')
-					{
-						actorF += character;
-						character = infile.get();
-					}
-
-					//get actor's last name from txt file
-					string actorL;
-					character = infile.get();
-
-					//build actor's last name
-					while (character != '\n')
-					{
-						actorL += character;
-						character = infile.get();
-					}
-
-					
-
-					//does item exist?
-					Item* found = getClassic(month, year, actorF, actorL);
-
-					//Item was found
-					if (found != NULL)
-					{
-						//set the found item in rental
-						rental->setItem(found);
-
-
-						//store completed transaction in history
-						list<Transaction*>  temp = history.at(rental->getCustID());
-						temp.push_back(rental);
-					}
-
-					//item not found
-					else
-					{
-						//display error message with the rejected line
-						cout << "Non-Existent video entered. Invalid Line: C " << month << " " <<
-							actorF << " " << actorL << " " << endl;
-					}
-
-					
-
-
-				}
-				
-				//using correct format for Drama Movie
-				else if (genre == 'D')
-				{
-					//get director from txt file
-					string director;
-					char character = infile.get();
-
-					//build title until comma is reached
-					while (character != ',')
-					{
-						director += character;
-						character = infile.get();
-					}
-					
-					//get title from txt file
-					string title;
-					character = infile.get();
-
-					//build title until comma is reached
-					while (character != ',')
-					{
-						title += character;
-						character = infile.get();
-					}
-
-					//does item exist?
-					Item* found = getDrama(director, title);
-					
-					//Item was found
-					if (found != NULL)
-					{
-						//set the found item in rental
-						rental->setItem(found);
-
-						//store completed transaction in history
-						list<Transaction*>  temp = history.find(rental->getCustID())->second;
-						temp.push_back(rental);
-					}
-
-					//item not found
-					else
-					{
-						//prints error message with rejected line
-						cout << "Non-Existent video entered. Invalid Line: D " << director << " " << title << endl;
-					}
-
-				}
-				
-				//using correct format for Comedy Movie
-				else if (genre == 'F')
-				{
-					//get title from txt file
-					string title;
-					char character = infile.get();
-					
-					//build title from infile
-					while (character != ',')
-					{
-						title += character;
-						character = infile.get();
-					}
-
-					//get year from infile
-					int year;
-					infile >> year;
-
-					//does item exist?
-					Item* found = getComedy(title, year);
-					
-					//Yes: Item is found
-					if (found != NULL)
-					{
-						//set the found item in rental
-						rental->setItem(found);
-
-						//store completed transaction in history
-						list<Transaction*>  temp = history.find(rental->getCustID())->second;
-						temp.push_back(rental);
-					}
-
-					//No: item not found
-					else
-					{
-						//print out error message with the line
-						cout << "Non-Existent video entered. Invalid Line: F " << title << ", " << year
-							<< endl;
-					}
-				}
-
-				//Invalid movie! (Genre code is incorrect)
-				else
-				{
-					//print out error message with the line
-					cout << "Non-Existent video entered. Invalid Line: " << "B " << rental->getCustID() << " " <<
-						media << " " << genre << " ";
-
-					//print out the rest of the line
-					while (!infile.eof() && infile.get() != '\n')
-					{
-						cout << infile.get();
-					}
-
-					//spacing
-					cout << endl;
-				}
-
-			}
-
-			else //NO: Customer does not exist
-			{
-				//print out error message with the line
-				cout << "Non-Existent customer entered. Invalid Line: " << "H " << custID << endl;
-				
-				//gets rid of '\n'
-				infile.get();
-			}
-
+			//Process & create a borrowed Transaction
+			createBTransaction(rental, infile);
 		}
 
 		//R: Set Transaction as returned
@@ -353,6 +126,245 @@ void Store::processCommands(ifstream& infile)
 	delete rental;
 	rental = NULL;
 	}
+}
+
+//-----------------createBorrowedTransaction--------------------------
+//Takes a Transaction Pointer and infile from the processCommands()
+//Verifies Customer ID with the customers in the database
+//Determines which genre movie is being rented and verifies that movie exists
+void Store::createBTransaction(Transaction* &rental, ifstream& infile)
+{
+	//read in Customer's ID from infile
+	int custID;
+	infile >> custID;
+
+	//returns NULL if customer doesn't exist
+	Customer* cust = getCustomer(custID);
+
+	//check to see if customer exists
+	if (cust != NULL) //YES
+	{
+		//set returned as false
+		rental->setBorrowed();
+
+		//set Customer's ID in the Transaction
+		rental->setCustID(custID);
+
+		//set the MediaType in the Transaction
+		char media;
+		infile >> media;
+		rental->setMediaType(media);
+
+		//get genre from infile
+		char genre;
+		infile >> genre;
+
+		//using correct format for Classic Movie
+		if (genre == 'C')
+		{
+			//get month from infile
+			int month;
+			infile >> month;
+
+			//get year from infile
+			int year;
+			infile >> year;
+
+			//get actor first name from txt file
+			string actorF;
+			char character = infile.get();
+
+			//build actor's first name until space
+			while (character != ' ')
+			{
+				actorF += character;
+				character = infile.get();
+			}
+
+			//get actor's last name from txt file
+			string actorL;
+			character = infile.get();
+
+			//build actor's last name
+			while (character != '\n')
+			{
+				actorL += character;
+				character = infile.get();
+			}
+
+			//does item exist?
+			Item* found = getClassic(month, year, actorF, actorL);
+
+			//Item was found
+			if (found != NULL)
+			{
+				//set the found item in rental
+				rental->setItem(found);
+
+
+				//store completed transaction in history
+				list<Transaction*>  temp = history.at(rental->getCustID());
+				temp.push_back(rental);
+			}
+
+			//item not found
+			else
+			{
+				//display error message with the rejected line
+				cout << "Non-Existent video entered. Invalid Line: C " << month << " " <<
+					actorF << " " << actorL << " " << endl;
+			}
+
+		}
+
+		//using correct format for Drama Movie
+		else if (genre == 'D')
+		{
+			//get director from txt file
+			string director;
+			char character = infile.get();
+
+			//build title until comma is reached
+			while (character != ',')
+			{
+				director += character;
+				character = infile.get();
+			}
+
+			//get title from txt file
+			string title;
+			character = infile.get();
+
+			//build title until comma is reached
+			while (character != ',')
+			{
+				title += character;
+				character = infile.get();
+			}
+
+			//does item exist?
+			Item* found = getDrama(director, title);
+
+			//Item was found
+			if (found != NULL)
+			{
+				//set the found item in rental
+				rental->setItem(found);
+
+				//store completed transaction in history
+				list<Transaction*>  temp = history.find(rental->getCustID())->second;
+				temp.push_back(rental);
+			}
+
+			//item not found
+			else
+			{
+				//prints error message with rejected line
+				cout << "Non-Existent video entered. Invalid Line: D " << director << " " << title << endl;
+			}
+
+		}
+
+		//using correct format for Comedy Movie
+		else if (genre == 'F')
+		{
+			//get title from txt file
+			string title;
+			char character = infile.get();
+
+			//build title from infile
+			while (character != ',')
+			{
+				title += character;
+				character = infile.get();
+			}
+
+			//get year from infile
+			int year;
+			infile >> year;
+
+			//does item exist?
+			Item* found = getComedy(title, year);
+
+			//Yes: Item is found
+			if (found != NULL)
+			{
+				//set the found item in rental
+				rental->setItem(found);
+
+				//store completed transaction in history
+				list<Transaction*>  temp = history.find(rental->getCustID())->second;
+				temp.push_back(rental);
+			}
+
+			//No: item not found
+			else
+			{
+				//print out error message with the line
+				cout << "Non-Existent video entered. Invalid Line: F " << title << ", " << year
+					<< endl;
+			}
+		}
+
+		//Invalid movie! (Genre code is incorrect)
+		else
+		{
+			//print out error message with the line
+			cout << "Non-Existent video entered. Invalid Line: " << "B " << rental->getCustID() << " " <<
+				media << " " << genre << " ";
+
+			//print out the rest of the line
+			while (!infile.eof() && infile.get() != '\n')
+			{
+				cout << infile.get();
+			}
+
+			//spacing
+			cout << endl;
+		}
+
+	}
+
+	else //NO: Customer does not exist
+	{
+		//print out error message with the line
+		cout << "Non-Existent customer entered. Invalid Line: " << "H " << custID << endl;
+
+		//gets rid of '\n'
+		infile.get();
+	}
+}
+
+void Store::processHistory(ifstream& infile)
+{
+	//Label for displaying Transactions of a particular Customer
+	cout << "----------------------Customer Transactions------------------" << endl;
+
+	//read in Customer's ID from infile
+	int custID;
+	infile >> custID;
+
+	//determine if customer exists in the system
+	Customer* temp = getCustomer(custID);
+
+	//check to see if customer exists
+	if (temp != NULL) //YES
+	{
+		//display customer's information
+		cout << "Customer ID: " << custID << " " << temp->getFName() << " " << temp->getLName() << endl;
+
+		//display customer's history
+		displayHistory(custID);
+	}
+
+	else //NO
+	{
+		//display error message with rejected line
+		cout << "Non-Existent customer entered. Invalid Line: " << "H " << custID << endl;
+	}
+
+	//gets rid of '\n'
+	infile.get();
 }
 
 //Displays the history of transactions (Checkouts and Returns) for customer with given id
